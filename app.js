e8edd01fb583de7f0c19f33d7d25197a5a805e20
John Pogosyan <jpogosyan@cornerstoneist.com>
2013-05-30 16:27:38 -0700

    Updated  variable for Heroku

--- app.js
+++ app.js
@@ -47,6 +47,6 @@ app.get('/freshbooks-api', routes.freshbooksapi);
 app.use(function(req, res, next){
   res.render('404.jade', {title: "404 - Page Not Found", showFullNav: false, status: 404, url: req.url });
 });
-app.listen(8080, function(){
+app.listen(process.env.PORT || 8080, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
